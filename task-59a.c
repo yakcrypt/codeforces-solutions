cf776a44fc6ecace51d0aee37046aebe7b2353e2
Kyrill Iakovenko <83785189+yakcrypt@users.noreply.github.com>
2026-01-08 21:25:42 +0300

     Added solution for task-59a.

---
+++ task-59a.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+enum {FIXED = 1000};
+
+int
+count_regs(char *str, int len)
+{
+    int cnt_lowers, cnt_uppers;
+    cnt_lowers = cnt_uppers = 0;
+    for (int i = 0; i < len; ++i) if (str[i] == tolower(str[i])) cnt_lowers++;
+    for (int j = 0; j < len; ++j) if (str[j] == toupper(str[j])) cnt_uppers++;
+    return (cnt_lowers > cnt_uppers) ? 1 : 0;
+}
+
+int 
+main(void)
+{
+    char str[FIXED]; scanf("%s", str);
+    int len = strlen(str);
+    for (int i = 0; i < len; ++i) {
+        if (count_regs(str, len)) {
+            str[i] = tolower(str[i]);
+        } else {
+            str[i] = toupper(str[i]);
+        }
+    }
+    printf("%s\n", str);
+    return 0;
+}
